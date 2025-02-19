145a706b283ff54fd3111cd1d471306fe9525cb7
piazorrilla <xmariapia9@gmail.com>
2025-02-19 12:18:39 +0000

    hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/**
+/** 
  * main - print programing is like building a mulrilingual puzzle 
- * return:0
+ * return: 0
  */
 
 int main(void)
 {
 	puts("\"programing is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
