8d8bfb3afa1acdeb0f2304ca3743f3dc8576d4c2
piazorrilla <xmariapia9@gmail.com>
2025-02-19 12:27:45 +0000

    hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/** 
- * main - print programing is like building a mulrilingual puzzle 
- * return: 0
+/**
+ * main - print programing is like building a multilingual puzzle
+ * Return:0
  */
 
 int main(void)
